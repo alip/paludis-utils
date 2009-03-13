f19cc3b1c86c8d2b25d725da2586a7e5eb737236
Ali Polatel <polatel@gmail.com>
2009-03-13 20:06:04 +0200

    fix docstring

--- putils/content.py
+++ putils/content.py
@@ -17,7 +17,7 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA
 
-"""Package related functions
+"""Package content related functions
 """
 
 from __future__ import generators
